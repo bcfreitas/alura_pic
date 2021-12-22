5533e1d2a54fd2665abe2f2ba19642074e8a9980
Bruno Freitas <bcfreitas@gmail.com>
2021-12-22 13:52:23 -0300

    chamada para consumir dados de API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,14 @@ export class AppComponent {
   photos = [];
   constructor(http: HttpClient){
     console.log(http);
+
+    //o metodo get do HttpClient retorna um Observable da lib RxJs,
+    //que possibilita chamadas assincronas e baseadas em eventos.
+    http
+      .get<Object[]>('http://localhost:3000/flavio/photos')
+      .subscribe(
+        photos => this.photos = photos,
+        err => console.log(err.message)
+      );
   }
 }
