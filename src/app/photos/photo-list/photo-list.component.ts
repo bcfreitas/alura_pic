db00e27ee1433541f8904a4893059c5b64e20a59
Bruno Freitas <bcfreitas@gmail.com>
2022-01-09 14:49:20 -0300

    Subject e debounceTime do RxJs

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
 import { Photo } from '../photo/photo';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-list',
@@ -11,6 +13,7 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   //injetando dependência do ActivatedRoute para ter acesso a variáveis da rota ativa
   constructor(private activatedRoute: ActivatedRoute){''}
@@ -19,5 +22,8 @@ export class PhotoListComponent implements OnInit {
   //e passamos o restante do código para o método do ciclo de vida ngOnInit()
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter2 => this.filter = filter2);
   }
 }
