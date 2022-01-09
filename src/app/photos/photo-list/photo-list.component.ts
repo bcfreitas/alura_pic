7ec54880e0cf4c5548403aefa2e35b583944ec6f
Bruno Freitas <bcfreitas@gmail.com>
2022-01-09 14:17:28 -0300

    uso de Resolver para carregar dados previamente antes de renderizar componente

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -14,16 +13,11 @@ export class PhotoListComponent implements OnInit {
   filter: string = '';
 
   //injetando dependência do ActivatedRoute para ter acesso a variáveis da rota ativa
-  constructor(private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute){''}
+  constructor(private activatedRoute: ActivatedRoute){''}
 
   //isolamos o construtor apenas para injecao de dependencia,
   //e passamos o restante do código para o método do ciclo de vida ngOnInit()
   ngOnInit(): void {
-
-    const user = this.activatedRoute.snapshot.params.userName;
-    this.photoService
-    .listFromUser(user)
-    .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data['photos'];
   }
 }
---
+++ src/app/photos/photo-list/photo-list.resolver.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { Observable } from "rxjs";
+import { Photo } from "../photo/photo";
+import { PhotoService } from "../photo/photo.service";
+
+@Injectable({providedIn:'root'})
+//o ipo do Resolve é o mesmo do método que retorna os dados da classe PhotoService
+export class PhotoListResolver implements Resolve<Observable<Photo[]>>{
+
+  //o construtor indica o serviço que será utilizado.
+  constructor(private service: PhotoService){
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    const userName = route.params.userName;
+    return this.service.listFromUser(userName);
+  }
+
+}
